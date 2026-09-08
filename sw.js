de10c3de0b0b3e598dc3fc12878f39279778fd3f
mahtdy <mahtdy@gmail.com>
2026-09-07 19:23:34 -0600

    chore: Update all version references to 1.4.4

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker with advanced caching strategies
-const VERSION = '1.4.3';
-const CACHE_NAME = `motion-tracker-v27`;
+const VERSION = '1.4.4';
+const CACHE_NAME = `motion-tracker-v28`;
 const CORE_CACHE = `${CACHE_NAME}-core`;
 const CDN_CACHE = `${CACHE_NAME}-cdn`;
 const RUNTIME_CACHE = `${CACHE_NAME}-runtime`;
